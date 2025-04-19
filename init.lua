2188ae317186975431b3e365eb8fbefe5ff9fd99
dreamer <serendipitywgy@gmail.com>
2025-04-19 23:54:28 +0800

    [config] 更新lualine配置

--- init.lua
+++ init.lua
@@ -2,6 +2,6 @@ local plugins = require("config")
 
 require("plugins.utils.root").setup()
 require("plugins.utils.run").setup()
-
+require("config.neovide").setup()
 _G.Root = require("plugins.utils.root")
 _G.Pick = require("plugins.utils.pick")
--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -10,15 +10,12 @@ return {
             component_separators = { left = "", right = "" },
             section_separators = { left = "", right = "" },
         },
-        extensions = { "nvim-tree" },
+        -- extensions = { "nvim-tree" },
         sections = {
             lualine_b = { "branch", "diff" },
-            lualine_x = {
-                "filesize",
-                "encoding",
-                "filetype",
-            },
+            lualine_x = { "filesize","encoding","filetype" },
+            lualine_y = { 'progress' }, -- 当前所在行数占总行数的百分比
+            lualine_z = { 'location' } -- 当前所在的行数和列数
         },
     },
 }
-
