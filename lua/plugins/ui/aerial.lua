228535fd516979a302ec7a354b62d3108a6338e2
wangguangyu <wangguangyu@maker-ray.com>
2025-04-23 22:48:31 +0800

    [config] 更新aerial插件

---
+++ lua/plugins/ui/aerial.lua
@@ -0,0 +1,89 @@
+return {
+    "stevearc/aerial.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+        require("aerial").setup({
+            -- 优先使用 LSP，其次使用 Treesitter
+            backends = { "lsp", "treesitter", "markdown", "man" },
+
+            -- 布局选项
+            layout = {
+                -- 默认宽度
+                default_direction = "right",
+                -- 宽度
+                width = 30,
+                -- 最小宽度
+                min_width = 20,
+            },
+
+            -- 显示导航箭头
+            show_guides = true,
+
+            -- 在打开文件时自动打开 aerial
+            -- 设为 false 可以手动控制
+            open_automatic = false,
+
+            -- 自动关闭没有符号的缓冲区
+            close_automatic_events = { "unsupported" },
+
+            -- 高亮当前项目
+            highlight_on_hover = true,
+
+            -- 自动聚焦到当前位置
+            autojump = true,
+
+            -- 过滤显示的符号
+            filter_kind = {
+                "Class",
+                "Constructor",
+                "Enum",
+                "Function",
+                "Interface",
+                "Method",
+                "Struct",
+                "Namespace",
+            },
+
+            -- 图标设置
+            icons = {
+                Class = "󰠱 ",
+                Constructor = " ",
+                Enum = " ",
+                Function = "󰊕 ",
+                Interface = " ",
+                Method = "󰆧 ",
+                Struct = "󰙅 ",
+                Namespace = "󰌗 ",
+            },
+
+            -- 自定义 LSP 优先级
+            lsp = {
+                -- 优先使用 clangd 作为 C++ 的 LSP
+                priority = {
+                    "clangd",
+                    "pyright",
+                    "lua_ls",
+                },
+            },
+
+            -- 键位绑定
+            keymaps = {
+                ["<CR>"] = "actions.jump",
+                ["<C-v>"] = "actions.jump_vsplit",
+                ["<C-s>"] = "actions.jump_split",
+                ["p"] = "actions.scroll",
+                ["<C-j>"] = "actions.down_and_scroll",
+                ["<C-k>"] = "actions.up_and_scroll",
+            },
+        })
+
+        -- 设置命令
+        vim.keymap.set("n", "<leader>cs", "<cmd>AerialToggle!<CR>", { desc = "打开/关闭代码大纲" })
+
+        -- 可选：在启动时自动打开 aerial
+        -- vim.cmd("autocmd FileType * AerialOpen")
+    end,
+}
--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -2,6 +2,7 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = {
         "nvim-tree/nvim-web-devicons",
+        "stevearc/aerial.nvim",
     },
     event = "VeryLazy",
     opts = {
@@ -13,9 +14,43 @@ return {
         -- extensions = { "nvim-tree" },
         sections = {
             lualine_b = { "branch", "diff" },
-            lualine_x = { "filesize","encoding","filetype" },
+            lualine_x = { "filesize", "encoding", "filetype" },
             lualine_y = { 'progress' }, -- 当前所在行数占总行数的百分比
-            lualine_z = { 'location' } -- 当前所在的行数和列数
+            lualine_z = { 'location' }, -- 当前所在的行数和列数
+            lualine_c = {               -- 添加一个新的组件来显示当前函数名
+                {
+                    -- function()
+                    --     local aerial = require("aerial")
+                    --     local symbol_info = aerial.get_location()
+                    --
+                    --     -- 添加调试信息，使用 vim.notify 输出 symbol_info
+                    --     if symbol_info then
+                    --         vim.notify("aerial.get_location() 返回: " .. vim.inspect(symbol_info), vim.log.levels.INFO,
+                    --             { title = "Lualine Debug" })
+                    --     else
+                    --         vim.notify("aerial.get_location() 返回: nil", vim.log.levels.INFO, { title = "Lualine Debug" })
+                    --     end
+                    --
+                    --     if symbol_info and symbol_info.name then
+                    --         return symbol_info.name
+                    --     end
+                    --     return "No Function"
+                    -- end,
+                    -- color = { fg = "#ffffff", bg = "#444444" }, -- 可选：自定义颜色
+                    -- padding = { left = 1, right = 1 },          -- 可选：自定义 padding
+                    --
+                    -- function()
+                    --     local aerial = require("aerial")
+                    --     local symbols = aerial.get_location()
+                    --     if symbols and #symbols > 0 then
+                    --         return symbols[1].name
+                    --     end
+                    --     return "No Function"
+                    -- end,
+                    -- color = { fg = "#ffffff", bg = "#444444" }, -- 可选：自定义颜色
+                    -- padding = { left = 1, right = 1 },          -- 可选：自定义 padding
+                },
+            },
         },
     },
 }
